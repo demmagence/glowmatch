4c8260f23656773ef6dc3bcd745c06e27b2e8f55
wibisanabama <wibisanabama@gmail.com>
2026-06-24 10:58:57 +0700

    fix: update routine viewmodel and streak tests to mock SharedPreferences and invoke completeRoutine manually

--- test/features/home/home_screen_test.dart
+++ test/features/home/home_screen_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import 'package:glowmatch/core/services/supabase_service.dart';
 import 'package:glowmatch/core/viewmodels/auth_viewmodel.dart';
@@ -31,6 +32,7 @@ Widget _buildHomeDark(RoutineViewModel routineVm) {
 
 void main() {
   setUpAll(() async {
+    SharedPreferences.setMockInitialValues({});
     final svc = SupabaseService();
     svc.resetForTesting();
     await svc.initialize(url: 'YOUR_URL', anonKey: 'YOUR_KEY');
